6c8444fe84960cf14d12e50b7846d197916d0f9a
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 14:00:13 -0800

    update numberToWord to handle numbers up to 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 var numberToWord = function(number) {
   var singleDigit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
   var teenDigits = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  var tens = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty' ,'seventy', 'eighty', 'ninety'];
   var output = '';
   if(number > 10) {
     if(number < 20) {// teens
       output = teenDigits[number-10];
+    } else { // twenty plus
+      var numArray = number.toString().split('');
+      output = tens[parseInt(numArray[0])-2] + ' ' + singleDigit[parseInt(numArray[1])];
     }
     // break it up
 
