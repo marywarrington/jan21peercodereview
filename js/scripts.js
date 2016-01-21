b17f99b47079d1c448760cc14fa3d456ea507d22
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 13:33:05 -0800

    define numberToWord for single digit numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+var numberToWord = function(number) {
+  var singleDigit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  return singleDigit[number];
+}
+
 var wordPuzzler = function(userSentence) {
   return userSentence.replace(/[aeiouy]/gi, '-');
 };
