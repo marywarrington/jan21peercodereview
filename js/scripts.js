379f21c55f64e420549656cc3f3c20e5753b9201
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 13:46:29 -0800

    update numberToWord to handle teen digits

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 var numberToWord = function(number) {
   var singleDigit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  return singleDigit[number];
+  var teenDigits = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  var output = '';
+  if(number > 10) {
+    if(number < 20) {// teens
+      output = teenDigits[number-10];
+    }
+    // break it up
+
+  } else { // single digit
+    output = singleDigit[number];
+  }
+  return output;
 }
 
 var wordPuzzler = function(userSentence) {
