a2b59c5c506f1ffa7b990eafb46f395d0f17fe31
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 13:19:13 -0800

    write spec to handle upper case vowels

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,7 @@ describe('wordPuzzler', function() {
     expect(wordPuzzler('This is the word place')).to.equal('Th-s -s th- w-rd pl-c-');
   });
 
+  it('will replace vowels in upper and lower case', function() {
+    expect(wordPuzzler('ThIs is thE best sEntEnce plAce')).to.equal('Th-s -s th- b-st s-nt-nc- pl-c-');
+  });
 });
